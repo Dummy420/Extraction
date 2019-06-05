356045700971ec97e24e53912fd0d8edf8d5cfb2
Dummy420 <51370252+Dummy420@users.noreply.github.com>
2019-06-05 10:35:48 +0200

    Update

--- Select.php
+++ Select.php
@@ -1,12 +1,18 @@
 <?php
 require_once("functions.php");
 $ips = ip_range('132.166.44.1', '132.166.47.254');
-var_dump($ips);
+$i = 1;
+foreach ($ips as $ip) {
+    echo $i . ' ';
+    echo($ip);
+    echo '<br>';
+    $i ++;
+}
 
  ?>
  <head>
    <title>Selection particulière</title>
  </head>
  <body>
-   
+
  </body>
